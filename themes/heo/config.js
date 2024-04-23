5a224e3c1d218bab6c0e325a4cdc21f1a12f1282
tr1gv0n <30565986+tr1gv0n@users.noreply.github.com>
2024-04-23 14:54:55 +0800

    Update domin

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://milkcoff.com' },
-    { title: '撸羊毛', url: 'https://ark.milkcoff.com' },
+    { title: '欢迎来到我的博客', url: 'https://llaiai.com' },
+    { title: '撸羊毛', url: 'https://ark.llaiai.com' },
   ],
 
   // 英雄区左右侧组件颠倒位置
